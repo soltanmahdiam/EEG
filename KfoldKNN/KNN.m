e4f0c8c0f275862716b9cc506cd508f691400981
soltanmahdiam <soltaniinejad.mahdii@gmail.com>
2025-08-25 17:09:29 +0330

    Add files via upload

---
+++ KfoldKNN/KNN.m
@@ -0,0 +1,156 @@
+clear
+close all
+clc
+
+load ..\Data\pop.mat
+
+%% Classification
+Features = {'Variance';'Abolute Energy';'Auto Regression';'Shannon Entropy';'Hjorth Mobility'};
+ 
+cn = 0;
+for fi = 1:7
+
+    if fi==1 || fi ==2
+        continue
+    end
+
+    
+    cn = cn + 1;
+
+    indv.features = 0;
+    indv.label = 0;
+    Pop = repmat(indv,[numel(Pop2) 1]);
+    
+    for i=1:numel(Pop2)
+        pp = Pop2(i).features;
+        Pop(i).features = pp(fi,:)';
+        Pop(i).label = Pop2(i).label;
+    end
+    
+    k = 5;
+    Indxs = crossvalind('Kfold', [Pop.label]', k);
+    
+    for fol = 1:k
+        
+        tstIndxs = (Indxs == fol);
+        trnIndxs = ~tstIndxs;
+
+        Mdl = fitcknn([Pop(trnIndxs).features]',[Pop(trnIndxs).label]','NumNeighbors',25,...
+        'NSMethod','exhaustive','Distance','cityblock',...
+        'Standardize',0);
+        
+        KNNdata.KNNOut = predict(Mdl,[Pop(tstIndxs).features]');
+        
+
+        % Calc Error
+        
+        confmat = confusionmat(KNNdata.KNNOut,[Pop(tstIndxs).label]'); % where response is the last column in the dataset representing a class
+        TP = confmat(2, 2);
+        TN = confmat(1, 1);
+        FP = confmat(1, 2);
+        FN = confmat(2, 1);
+        
+        
+        Acc(fol) = (TP + TN) / (TP + TN + FP + FN);  %# ok
+        Sen(fol) = TP / (FN + TP);  %# ok
+        Spec(fol) = TN / (TN + FP);  %# ok
+        
+        
+        z = FP / (FP+TN);
+        X = [0;Sen(fol);1];
+        Y = [0;z;1];
+        AC(fol) = trapz(Y,X);  %# ok  % This way is used for only binary classification
+    end
+    Accuracymean(cn,1)    = mean(Acc);   %# ok
+    Accuracyvar(cn,1)     = var(Acc);    %# ok
+    Sensitivitymean(cn,1) = mean(Sen);   %# ok
+    Sensitivityvar(cn,1)  = var(Sen);    %# ok
+    Specificitymean(cn,1) = mean(Spec);  %# ok
+    Specificityvar(cn,1)  = var(Spec);   %# ok
+    AUCmean(cn,1)         = mean(AC);    %# ok
+    AUCvar(cn,1)          = var(AC);     %# ok
+  
+    
+    
+end
+Accuracy    = table(Accuracymean*100,Accuracyvar*100,'VariableNames',{'Mean','Variance'});
+Sensitivity = table(Sensitivitymean*100,Sensitivityvar*100,'VariableNames',{'Mean','Variance'});
+Specificity = table(Specificitymean*100,Sensitivityvar*100,'VariableNames',{'Mean','Variance'});
+AUC         = table(AUCmean*100,AUCvar*100,'VariableNames',{'Mean','Variance'});
+
+Meters = table(Features,Accuracy,Sensitivity,Specificity,AUC);
+T2 = inner2outer(Meters)
+% writetable(Meters,'KNN_Times.xlsx')
+TableWriter(T2,'KNN_Times.xlsx')
+
+%% Pop1
+
+Features = {'CorrPearson';'coherence';'PLI';'PLV'};
+clear Accuracy Specificity Sensitivity AUC Accuracymean Accuracyvar Sensitivitymean Sensitivityvar Specificitymean
+clear Specificityvar AUCmean AUCvar Acc Spec AC
+cn = 0;
+for fi = 1:4
+    cn = cn + 1;
+
+    indv.features = 0;
+    indv.label = 0;
+    Pop = repmat(indv,[numel(Pop2) 1]);
+    
+    for i=1:numel(Pop1)
+        pp = Pop1(i).features;
+        Pop(i).features = pp(:,fi);
+        Pop(i).label = Pop1(i).label;
+    end
+    
+
+    Indxs = crossvalind('Kfold', [Pop.label]', k);
+    
+    for fol = 1:k
+        
+        tstIndxs = (Indxs == fol);
+        trnIndxs = ~tstIndxs;
+
+        Mdl = fitcknn([Pop(trnIndxs).features]',[Pop(trnIndxs).label]','NumNeighbors',25,...
+        'NSMethod','exhaustive','Distance','cityblock',...
+        'Standardize',0);
+        
+        KNNdata.KNNOut = predict(Mdl,[Pop(tstIndxs).features]');
+        
+
+        % Calc Error
+        
+        confmat = confusionmat(KNNdata.KNNOut,[Pop(tstIndxs).label]'); % where response is the last column in the dataset representing a class
+        TP = confmat(2, 2);
+        TN = confmat(1, 1);
+        FP = confmat(1, 2);
+        FN = confmat(2, 1);
+        
+        
+        Acc(fol) = (TP + TN) / (TP + TN + FP + FN);  %# ok
+        Sen(fol) = TP / (FN + TP);  %# ok
+        Spec(fol) = TN / (TN + FP);  %# ok
+        
+        
+        z = FP / (FP+TN);
+        X = [0;Sen(fol);1];
+        Y = [0;z;1];
+        AC(fol) = trapz(Y,X);  %# ok  % This way is used for only binary classification
+    end
+    Accuracymean(cn,1)    = mean(Acc);   %# ok
+    Accuracyvar(cn,1)     = var(Acc);    %# ok
+    Sensitivitymean(cn,1) = mean(Sen);   %# ok
+    Sensitivityvar(cn,1)  = var(Sen);    %# ok
+    Specificitymean(cn,1) = mean(Spec);  %# ok
+    Specificityvar(cn,1)  = var(Spec);   %# ok
+    AUCmean(cn,1)         = mean(AC);    %# ok
+    AUCvar(cn,1)          = var(AC);     %# ok
+
+end
+Accuracy    = table(Accuracymean*100,Accuracyvar*100,'VariableNames',{'Mean','Variance'});
+Sensitivity = table(Sensitivitymean*100,Sensitivityvar*100,'VariableNames',{'Mean','Variance'});
+Specificity = table(Specificitymean*100,Sensitivityvar*100,'VariableNames',{'Mean','Variance'});
+AUC         = table(AUCmean*100,AUCvar*100,'VariableNames',{'Mean','Variance'});
+
+Meters = table(Features,Accuracy,Sensitivity,Specificity,AUC);
+T2 = inner2outer(Meters);
+TableWriter(T2,'KNN_App.xlsx')
